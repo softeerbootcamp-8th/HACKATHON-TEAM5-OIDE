5c5745de6a4ed55a788cf35b5cac15979157759d
Juhye0k <kyoung1678@naver.com>
2026-08-24 16:51:40 +0900

    feat : 프로젝트 초기 설정

---
+++ src/main/java/com/example/oide/OideApplication.java
@@ -0,0 +1,13 @@
+package com.example.oide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oide/OideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
