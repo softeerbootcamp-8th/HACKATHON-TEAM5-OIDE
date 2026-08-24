0ce66f4f07ae34fb6af9c1212448718992d6bf3d
Juhye0k <kyoung1678@naver.com>
2026-08-24 22:28:53 +0900

    refactor: Lombok Slf4j 적용

--- backend/src/main/java/com/example/oide/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/oide/global/exception/GlobalExceptionHandler.java
@@ -1,15 +1,13 @@
 package com.example.oide.global.exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
-	private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
 	@ExceptionHandler(BusinessException.class)
 	public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException exception) {
